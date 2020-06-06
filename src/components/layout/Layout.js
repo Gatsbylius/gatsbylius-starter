169ff89e2aa5ed3c12e522d6d347c406cc9c4249
Gaëtan Kueny <gaetan@occitech.fr>
2020-06-06 17:30:46 +0200

    chore(dependencies): Upgrade react-helmet to 6

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { ThemeProvider } from "styled-components";
 import { GoMarkGithub } from "react-icons/go";
 import Header from "components/layout/Header";
